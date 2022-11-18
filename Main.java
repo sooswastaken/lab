2c8bac13b9f097b986063223014fc7a743b32094
soos <62181284+sooswastaken@users.noreply.github.com>
2022-11-18 20:43:19 +0000

    Did the whole lab

--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello, World!");
+    printStars(20);
+    printMults(8, 100);
   }
+
+
+  // method printStars that, given an integer n prints n stars on a line with a space between each one.
+  public static void printStars(int n) {
+    for (int i = 0; i < n; i++) {
+      System.out.print("* ");
+    }
+    System.out.println();
+  }
+
+  // Write a method printMults that when passed an integer num and an integer limit will print all the multiples of num up to limit (inclusive) on one line with spaces between them.
+  public static void printMults(int num, int limit) {
+    for (int i = 1; i <= limit; i++) {
+      if (i % num == 0) {
+        System.out.print(i + " ");
+      }
+    }
+    System.out.println();
+  }
+
 }
